be851cc2169ad45cf81408d30f7216ff672b49d4
RichardMarkRoss <cptrichkidprice@gmail.com>
2019-12-06 14:44:30 +0200

    heroku push

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,6 @@
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
+//import waiters.ShiftDays;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -7,13 +8,25 @@ import java.util.Map;
 import static spark.Spark.*;
 
 public class HelloWorld {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
     public static void main(String[] args) {
+
+        port(getHerokuAssignedPort());
+
         get("/", (req,  res) -> {
             Map<String, Object> model= new HashMap<> ();
             return new HandlebarsTemplateEngine ().render (new ModelAndView (model,"index.handlebars"));
         });
 
-        get("/home", (req,  res) -> {
+        post("/home", (req,  res) -> {
+
             Map<String, Object> model= new HashMap<> ();
             return new HandlebarsTemplateEngine ().render (new ModelAndView (model,"home.handlebars"));
         });
